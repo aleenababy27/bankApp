0e65dd8ffb862336598c51949c2985f56e497b18
Aleena <aleenababy27@gmail.com>
2022-04-01 13:29:24 +0530

    loginpage and onchange update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+
+  aim="Your Perfect Banking Partner";
+  accnum="Please enter your Account number"
+  acno=""
+  pwd=""
+
+  // dataBase
+
+  dataBase={
+
+      1000:{acno:1000,uname:"Aleena",password:1000,balance:750},
+      1001:{acno:1001,uname:"Mariya",password:1001,balance:1500},
+      1002:{acno:1000,uname:"Augustine",password:1002,balance:50000}
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  passwordChange(event:any){
+    this.pwd=event.target.value;
+    console.log(this.pwd);
+    
+  }
+  acnoChange(event:any){
+    this.acno=event.target.value;
+    console.log(this.acno);
+    
+  }
+
+
+  login(){
+    alert("Login attempted")
   }
 
 }
